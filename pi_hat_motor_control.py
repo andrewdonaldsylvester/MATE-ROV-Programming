fc83df5260cb97cfd1a6046b193beb6788ff3155
Funny Dog <35876206+andrewdonaldsylvester@users.noreply.github.com>
2022-01-09 15:54:49 -0800

    Create pi_hat_motor_control.py
    Library for using a Sparkfun servo hat to control motors

---
+++ pi_hat_motor_control.py
@@ -0,0 +1,46 @@
+import time
+import pi_servo_hat
+
+
+def map_value(value, in_min, in_max, out_min, out_max):
+    return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+class PiHat:
+    def __init__(self):
+        self.hat = pi_servo_hat.PiServoHat()
+        self.hat.restart()
+
+    def send_pwm(self, channel, signal):
+        # signal (in microseconds for PWM)
+        
+        angle = (signal - 922) / 10.8
+        
+        self.hat.move_servo_position(channel, angle)
+
+
+    def drive_motor(self, channel, velocity):
+        # velocity is a float between -1 and 1.
+        # -1 corresponds to full backwards,
+        # 1 corresponds to full forwards,
+        # 0 corresponds to stopped
+        
+        velocity = max(min(velocity, 1), -1)
+        velocity = map_value(velocity, -1, 1, 1000, 2000)
+        
+        self.send_pwm(channel, velocity)
+    
+    def stop_all_motors(self):
+        for channel in range(16):
+            self.drive_motor(channel, 0)
+
+if __name__ == "__main__":
+  my_hat = PiHat()
+
+  for motor_vel in range(-100, 100, 2):
+      print(motor_vel/100)
+      my_hat.drive_motor(0, motor_vel/100)
+      time.sleep(0.1)
+
+  my_hat.stop_all_motors()
+
