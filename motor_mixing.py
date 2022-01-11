81905c89d1722a01658324298122337069d31a85
Funny Dog <35876206+andrewdonaldsylvester@users.noreply.github.com>
2022-01-11 14:51:38 -0800

    Update motor_mixing.py
    Is able to drive a mecanum land rover programmatically.

--- motor_mixing.py
+++ motor_mixing.py
@@ -2,15 +2,30 @@ from pi_hat_motor_control import PiHat
 import time
 
 my_hat = PiHat()
-for _ in range(5):
-    for motor_vel in range(-100, 100, 2):
-        print(motor_vel/100)
-        my_hat.drive_motor(0, motor_vel/100)
-        #my_hat.drive_motor(1, motor_vel/100)
-        my_hat.drive_motor(2, motor_vel/100)
-        #my_hat.drive_motor(3, motor_vel/100)
 
-        time.sleep(0.1)
+
+def drive(forward_vel, strafe_vel, turn_vel):
+    motor1_vel =  forward_vel - strafe_vel - turn_vel
+    motor2_vel =  forward_vel + strafe_vel - turn_vel
+    motor3_vel = -forward_vel - strafe_vel - turn_vel
+    motor4_vel = -forward_vel + strafe_vel - turn_vel
+
+    my_hat.drive_motor(0, motor1_vel)
+    my_hat.drive_motor(1, motor2_vel)
+    my_hat.drive_motor(2, motor3_vel)
+    my_hat.drive_motor(3, motor4_vel)
+
+for _ in range(10):
+    drive(1, 0, 0)
+    time.sleep(1)
+    drive(0, 0, 0.7)
+    time.sleep(2.5)
+    drive(0, 1, 0)
+    time.sleep(1)
+    drive(0, 0, 0.7)
+    time.sleep(2)
+    drive(0.6, 0.1, 0.3)
+    time.sleep(4)
+
     
 my_hat.stop_all_motors()
-
