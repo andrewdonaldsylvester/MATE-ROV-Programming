ad834765d2bed9295959cfd0de5cdc3baac775ee
Funny Dog <35876206+andrewdonaldsylvester@users.noreply.github.com>
2023-04-20 15:38:34 -0700

    Update udp-server.py

--- udp-server.py
+++ udp-server.py
@@ -1,45 +1,240 @@
+from serial import Serial
 import socket
+import pickle
+import glob
 import time
-import json
-localIP = "127.0.0.1"
+# Look up serial port for arduino
+# Connect to Pi first, then run app
 
-localPort = 20001
+
+def invert_channel(value):
+    return 3000 - value
+
+
+def shrink_channel(value, scale):
+    return 1500 + (value - 1500) / scale
+
+
+def map_value(x, in_min, in_max, out_min, out_max):
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+def clamp(x, in_min, in_max):
+    return max(in_min, min(x, in_max))
+
+
+def clamp_if_near(x, clamp_value, epsilon, return_value=1500):
+    if abs(x - clamp_value) <= epsilon:
+        return return_value
+    else:
+        return x
+
+
+ip = "192.168.1.2" # ROV IP!!!
+
+serverAddressPort = (ip, 20001)
 
 bufferSize = 128
 
-msgFromServer = "Hello UDP Client"
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-bytesToSend = str.encode(msgFromServer)
+send_message = pickle.dumps([1500, 1500, 1500, 1500,
+                                 1500, 1500, 1500, 1500,
+                                 1500, 1500, 1500, 1500,
+                                 1500, 1500, 1500, 1500])
 
-# Create a datagram socket
+try:
+    UDPClientSocket.sendto(send_message, serverAddressPort)
+except:
+    print("Connection Lost, reconnecting...")
+    UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    # print("Reconnected!")
 
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+arduino_port = glob.glob('/dev/cu.usbmodem*')[0]
+ser = Serial(arduino_port, 115200)
 
-# Bind to address and ip
+# flashlight_state = False
+# pump_state = False
 
-UDPServerSocket.bind((localIP, localPort))
+try:
+    if ser.is_open:
 
-print("UDP server up and listening")
+        send_message = pickle.dumps([1500, 1500, 1500, 1500,
+                                     1500, 1500, 0, 0,
+                                     0, 0, 0, 0,
+                                     0, 0, 0, 0])
 
-# Listen for incoming datagrams
-while True:
+        UDPClientSocket.sendto(send_message, serverAddressPort)
 
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+        FLASHLIGHT_SIG = 0
+        PUMP_SIG = 0
+        SWITCH_SIG = 1000
 
-    message = bytesAddressPair[0]
-    address = bytesAddressPair[1]
+        while True:
+            values = ser.readline()
+            channels = values.split()
 
-    clientMsg = "Message from Client:{}".format(message)
-    clientIP = "Client IP Address:{}".format(address)
+            print(channels)
 
-    if (count % 1000) == 0:
-        print(clientMsg)
-        print(clientIP)
-        data = json.loads(message.decode('utf-8'))
-        print(data)
+            if len(channels) < 6:
+                continue
+
+            L_X = invert_channel(int(channels[2])) - 7
+            L_Y = int(channels[5]) - 25
+            R_X = int(channels[4])
+            R_Y = int(channels[3])
+            # R_Y = 1425
+
+            BINARY_SWITCH = int(channels[1])
+
+            if 800 <= BINARY_SWITCH <= 1500:
+                SWITCH_SIG = 1000
+            elif 1501 <= BINARY_SWITCH <= 2200:
+                SWITCH_SIG = 2250
+
+            TERNARY_SWITCH = int(channels[0])
+
+            if 800 <= TERNARY_SWITCH <= 1200:
+                # flashlight_state = False
+                # pump_state = True
+
+                FLASHLIGHT_SIG = 0
+                PUMP_SIG = 18777
+            elif 1300 <= TERNARY_SWITCH <= 1700:
+                # flashlight_state = False
+                # pump_state = False
+
+                FLASHLIGHT_SIG = 0
+                PUMP_SIG = 0
+            elif 1800 <= TERNARY_SWITCH <= 2200:
+                # flashlight_state = True
+                # pump_state = False
+
+                FLASHLIGHT_SIG = 15000
+                PUMP_SIG = 0
 
 
 
-    # Sending a reply to client
+            # print("L_X {} L_Y {} R_X {} R_Y {} BINARY_SWITCH {} TERNARY_SWITCH {}"
+            #       .format(L_X, L_Y, R_X, R_Y, BINARY_SWITCH, TERNARY_SWITCH))
 
-    UDPServerSocket.sendto(bytesToSend, address)
+            # forward = R_Y
+            # strafe = R_X
+            # turn = L_X
+            # altitude = L_Y
+
+            forward = R_Y
+            strafe = L_X
+            turn = R_X
+            altitude = L_Y
+
+            # speed_modifier = {"LO": 3, "MID": 2, "HI": 1}
+
+            # M1 = forward - strafe - shrink_channel(turn, 2) + 3000
+            # M2 = forward + strafe + shrink_channel(turn, 2) - 3000
+            # M3 = forward + strafe - shrink_channel(turn, 2)
+            # M4 = forward - strafe + shrink_channel(turn, 2)
+
+            # Updated
+            M1 = (forward) - strafe - shrink_channel(turn, 2) + 3000
+            M2 = -(forward) + strafe - shrink_channel(turn, 2) + 3000
+            M3 = -(forward) - strafe + shrink_channel(turn, 2) + 3000
+            M4 = (forward) + strafe + shrink_channel(turn, 2) - 3000
+
+            # M1 = forward + strafe + shrink_channel(turn, 2) - 3000
+            # M2 = forward - strafe - shrink_channel(turn, 2) + 3000
+            # M3 = -forward + strafe - shrink_channel(turn, 2) + 3000
+            # M4 = -forward - strafe + shrink_channel(turn, 2) + 3000
+
+            # Motor      1 2 3 4
+            # Forward    + + - -
+            # Right      + - + -
+            # Clockwise  + - - +
+
+            # if abs(altitude - 1500) <= 100:
+            #     altitude = 1500
+
+
+            M5 = altitude
+            M6 = altitude
+
+            M1 = clamp(M1, 1100, 1900)
+            M2 = clamp(M2, 1100, 1900)
+            M3 = clamp(M3, 1100, 1900)
+            M4 = clamp(M4, 1100, 1900)
+            M5 = clamp(M5, 1100, 1900)
+            M6 = clamp(M6, 1100, 1900)
+
+            # Reverse Motors
+            M1 = map_value(M1, 1100, 1900, 1900, 1100)
+            M2 = map_value(M2, 1100, 1900, 1900, 1100)
+            # M3 = map_value(M3, 1100, 1900, 1100, 1900)
+            # M4 = map_value(M4, 1100, 1900, 1900, 1100)
+            # M5 = map_value(M5, 1100, 1900, 1100, 1900)
+            # M6 = map_value(M6, 1100, 1900, 1100, 1900)
+
+            M1 = map_value(M1, 1100, 1900, 1250, 1750)
+            M2 = map_value(M2, 1100, 1900, 1250, 1750)
+            M3 = map_value(M3, 1100, 1900, 1250, 1750)
+            M4 = map_value(M4, 1100, 1900, 1250, 1750)
+            M5 = map_value(M5, 1100, 1900, 1250, 1750)
+            M6 = map_value(M6, 1100, 1900, 1250, 1750)
+
+            M1 = round(clamp(M1, 1250, 1750))
+            M2 = round(clamp(M2, 1250, 1750))
+            M3 = round(clamp(M3, 1250, 1750))
+            M4 = round(clamp(M4, 1250, 1750))
+            M5 = round(clamp(M5, 1250, 1750))
+            M6 = round(clamp(M6, 1250, 1750))
+
+            e = 20
+
+            M1 = clamp_if_near(M1, 1500, e, return_value=1484)
+            M2 = clamp_if_near(M2, 1500, e, return_value=1484)
+            M3 = clamp_if_near(M3, 1500, e, return_value=1484)
+            M4 = clamp_if_near(M4, 1500, e, return_value=1484)
+            M5 = clamp_if_near(M5, 1500, e, return_value=1484)
+            M6 = clamp_if_near(M6, 1500, e, return_value=1484)
+
+            """ ADD CODE TO USE FLASHLIGHT AND PUMP STATES TO TOGGLE SWITCHES WITH THEIR CHANNELS """
+            """ PINS S6 LEFT UV FLASHLIGHT AND S7 RIGHT PUMP """
+            """ GRIPPER ON S9 """
+
+
+            # The strange order is there to rectify the wiring layout
+            send_message = pickle.dumps([M4, M3, M6, M5,
+                                         M2, M1, FLASHLIGHT_SIG, PUMP_SIG,
+                                         0, SWITCH_SIG, 0, 0,
+                                         0, 0, 0, 0])
+
+            # send_message = pickle.dumps([1500, 1500, 1500, 1500,
+            #                              1500, 1500, 0, 0,
+            #                              0, 0, 0, 0,
+            #                              0, 0, 0, 0])
+
+            # send_message = pickle.dumps([0, 0, 0, 0,
+            #                              0, 0, 0, 0,
+            #                              0, 0, 0, 0,
+            #                              0, 0, 0, 0])
+
+            try:
+                UDPClientSocket.sendto(send_message, serverAddressPort)
+            except:
+                print("Connection Lost, reconnecting...")
+                UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+                # print("Reconnected!")
+
+            print("M1 = {:5} \t M2 = {:5} \t M3 = {:5} \t M4 = {:5} \t M5 = {:5} \t M6 = {:5} \t S1 = {:5} \t Flashlight = {:5} \t Pump = {:5}"
+                  .format(round(M1), round(M2), M3, M4, M5, M6, SWITCH_SIG, FLASHLIGHT_SIG, PUMP_SIG))
+finally:
+
+    send_message = pickle.dumps([0, 0, 0, 0,
+                                 0, 0, 0, 0,
+                                 0, 0, 0, 0,
+                                 0, 0, 0, 0])
+    UDPClientSocket.sendto(send_message, serverAddressPort)
+
+
+    print("Closing Connection")
+
+    ser.close()
